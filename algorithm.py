373a283d3db1844298e9110a8d6cc53a02ae1922
Kayla Ford <Kayla.ford@mail.citytech.cuny.edu>
2018-05-16 18:32:53 -0400

    pushing project changes

---
+++ algorithm.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+List = [1,2,3,5,10,9,8,9,10,11,7]
+
+def findConsecutiveRuns(List):
+   runs =[]
+   order = 0 # keeping track of Ascending or Descending 
+   for index,value in enumerate(List): # utilizing indice, and value
+        if(index < len(List)-2):
+           current,next,nextnext = index,index+1,index+2
+           if(List[current]+1 == List[next]):
+               order =1
+           if(List[current]-1== List[next]):
+               order =-1
+           if((order == 1) and (List[next]+1 == List[nextnext])):
+               runs.append(current)
+           elif((order == -1 )and (List[next]-1 == List[nextnext])):
+               runs.append(current)
+
+   return runs
+
+print(findConsecutiveRuns(List))
